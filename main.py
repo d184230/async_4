ce7c197dc0d30ba8df3ff3e5ef5943128f8b624f
d184230 <d184230@gmail.com>
2021-07-13 22:38:26 +0300

    Save messages history to file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import asyncio
+import aiofiles
+from datetime import datetime
 
 
 async def chat_reader(host, port):
@@ -6,7 +8,11 @@ async def chat_reader(host, port):
     while True:
         message_bytes = await reader.readline()
         message = message_bytes.decode('utf8').strip()
-        print(message)
+        message_datetime = datetime.now()
+        message_with_datetime = f'[{message_datetime.strftime("%d.%m.%Y %H:%M")}] {message}'
+        print(message_with_datetime)
+        async with aiofiles.open('chat_log.txt', mode='a', encoding='utf8') as f:
+            await f.write(f'{message_with_datetime}\n')
 
 
 asyncio.run(chat_reader('minechat.dvmn.org', 5000))
